f19ae707ed338b1b8761af78aaca798117790da0
Eunbyul <las4134@gmail.com>
2023-07-05 15:22:56 +0100

    modify conflict

--- addTask.js
+++ addTask.js
@@ -9,7 +9,7 @@ function addUserTask() {
         taskItemHTML = 
         `<div class="item-list grid">
             <div class="outstanding-icon flex" tabindex="4">
-                <img class='medium-icon'src="./images/icons/outstanding.png" alt="" id='outstanding'>
+                <img class="outstanding-icon-img medium-icon" src="./images/icons/outstanding.png" alt="" id="outstanding">
             </div>
             <div class="list-txt" tabindex="5">
                 <p class="list-txt-title" tabindex="6">${userTaskTitle.value}</p>
@@ -17,11 +17,9 @@ function addUserTask() {
             </div>
         
             <div class="user-controls flex" tabindex="8">
-
                 <img tabindex="9" src="./images/user-controls/completed-control.png" class="completed-icon medium-icon">     
                 <img tabindex="10" src="./images/user-controls/edit-control.png" class="edit-icon medium-icon">       
                 <img tabindex="11" src="./images/user-controls/delete-control.png" class="delete-icon medium-icon">   
-
             </div>       
         </div> `;
 
@@ -32,48 +30,43 @@ function addUserTask() {
         searchTaskList.insertAdjacentHTML('beforeend', searchTaskItemHTML);
 
 
-      
-        let edits = document.querySelectorAll('.edit-icon');
-        let completedIcons = document.querySelectorAll('.completed-icon');
-        let outstandingIcons = document.querySelectorAll('.outstanding-icon');
-
-        const changeToCompleted = (e) => { 
-          const completedIcon = e.target
-          if(completedIcon){
-            const outstandingIcon = completedIcon.parentElement.parentElement.querySelector('.outstanding-icon img');
-            outstandingIcon.id = 'done'
-            outstandingIcon.src = './images/user-controls/completed-control.png';
-          }
-        }
-
-       const changeToOutstanding = (e) => {
-        const outStadningIcon = e.target;
-        outStadningIcon.id = 'outstanding'
-        outStadningIcon.src = './images/icons/outstanding.png';
-       }
-
-        completedIcons.forEach((completed)=> {
-            completed.addEventListener('click',  changeToCompleted)
-        })
-
-        outstandingIcons.forEach((outstanding)=> {
-            outstanding.addEventListener('click',  changeToOutstanding)
-        })
-
-
-
-        edits.forEach((edit)=> {
-            edit.addEventListener('click', function(){
-                //console.log('edit')
-               
-            })
-        })
-        
-        closePopUp();
-
         editButton();
         closeAddPopUp();
 
+
+let edits = document.querySelectorAll('.edit-icon');
+let completedIcons = document.querySelectorAll('.completed-icon');
+let outstandingIcons = document.querySelectorAll('.outstanding-icon-img');
+
+
+
+
+
+const changeToCompleted = (e) => { 
+    console.log('click')
+  const completedIcon = e.target
+  if(completedIcon){
+    const outstandingIcon = completedIcon.parentElement.parentElement.querySelector('.outstanding-icon img');
+    outstandingIcon.id = 'done'
+    outstandingIcon.src = './images/user-controls/completed-control.png';
+  }
+}
+
+const changeToOutstanding = (e) => {
+    console.log('ch')
+    const outStadningIcon = e.target;
+    outStadningIcon.id = 'outstanding'
+    outStadningIcon.src = './images/icons/outstanding.png';
+   }
+
+completedIcons.forEach((completed)=> {
+    completed.addEventListener('click',  changeToCompleted)
+})
+
+outstandingIcons.forEach((outstanding)=> {
+    outstanding.addEventListener('click',  changeToOutstanding)
+})
+        
     }
   };
 
@@ -82,6 +75,3 @@ addTaskForm.addEventListener('submit', (e) => {
     e.preventDefault();
     addUserTask();
 });
-
-
-
--- editTask.js
+++ editTask.js
@@ -76,15 +76,6 @@ function cancelButton() {
     })
 }
 
-
-
-function addListensToButton() {
-    let editButtons = document.querySelectorAll('.edit-icon');
-    for (let i = 0; i < editButtons.length; i++) {
-        editButtons[i].addEventListener('click', openEditPopup);
-    } 
-}
-
 function setEditPopContent() {
     console.log('open edit popup');
     let editTaskPop = `
