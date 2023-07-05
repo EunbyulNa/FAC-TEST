5e9357ea2b7f9c96fda22dfbcef98631241bd295
Eunbyul <las4134@gmail.com>
2023-07-05 15:30:17 +0100

    modify toggle function

--- toggleSwitch.js
+++ toggleSwitch.js
@@ -1,13 +1,20 @@
 
 let toggleSwitch = document.querySelector('#switch');
+let toggleMsg = document.querySelector('#toggle-msg')
 
 const showCompleted = () => {
   if(toggleSwitch.checked === true){
     console.log('on')
+    toggleMsg.textContent = "Show completed"
     document.querySelectorAll('#outstanding').forEach((out)=> {
       out.parentElement.parentElement.style.display = 'none'
     })
     
+  }else{
+    toggleMsg.textContent = "ALL"
+    document.querySelectorAll('#outstanding').forEach((out)=> {
+        out.parentElement.parentElement.style.display = 'grid'
+      })
   }
 }
 
